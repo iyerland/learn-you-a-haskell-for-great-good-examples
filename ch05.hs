c83e9dfe6267d6979f55d78a65ef6e9ef9dbb40e
Narayan Iyer <kode.sutra@gmail.com>
2011-08-27 23:37:32 -0500

    Added few more functions from Chapter 5

--- ch05.hs
+++ ch05.hs
@@ -35,3 +35,31 @@ largestDiv = head (filter p [10000, 9999..])
    where p x = x `mod` 3829 == 0
    
 -- find the sum of all odd squares that are smaller than 10000.
+-- sum (takeWhile (< 10000) (filter odd (map (^2) [1..])))
+
+-- For all starting numbers between 1-100, how many Collatz chain have
+-- length > 15
+-- Collatz Chain: 
+--	1. Start with a Natural Number
+--	2. If 1 then stop
+--	3. If even divide by 2
+--	4. If odd multiply by 3 + 1
+--  4. Repeat the algorithm with resulting number
+
+chain :: Integer -> [Integer]
+chain 1 = [1]
+chain n
+	| even n = n : chain (n `div` 2)
+	| odd  n = n : chain (n * 3 + 1)
+	
+numLongChains :: Int
+numLongChains = length (filter isLengthGreaterThanFifteen (map chain [1..100]))	
+	where isLengthGreaterThanFifteen xs = length xs > 15
+
+-- this uses anonymous function or lambda to flip its parameters (x,y) and then 
+-- apply the function f.
+
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f = \x y -> f y x
+
+
